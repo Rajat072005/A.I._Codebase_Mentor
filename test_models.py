48951c80525f5be161f02cf7f59a33863adf7527
Rajat Trehan <trehanrajat4@gmail.com>
2026-09-04 20:30:37 +0530

    deleted duplicate files

--- test_models.py
+++
@@ -1,9 +0,0 @@
-# import google.generativeai as genai
-
-# # 1. You must provide your API key before calling any other genai functions
-# genai.configure(api_key="AQ")
-
-# # 2. Now this will work without crashing!
-# print("Fetching available models...")
-# for model in genai.list_models():
-#     print(model.name)
